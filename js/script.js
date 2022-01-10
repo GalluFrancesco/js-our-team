2a8faf20896efc872e7e97ddcb761c83f56784b2
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-10 17:01:40 +0100

    Add: funzione che genera team-card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+//creo una funzione che riceve come argomenti
+//nome, ruolo e nome immagine
+//e restituisce una teamCard
+function newCard(name, role, img){
+  
+  //creo un elemento html div
+  let teamCard=document.createElement('div');
+  //aggiungo la classe team-card
+  teamCard.classList.add("team-card");
+
+  //creo un elemento html div
+  let cardImg=document.createElement('div');
+  //aggiungo al div la classe card-img
+  cardImg.classList.add("card-image");
+
+  //creo un elemento html img
+  let htmlImg=document.createElement('img');
+    //riempio il campo src dell'elemento div con il testo
+    //che ricevo come parametro nella funzione
+    htmlImg.src=`img/${img}`;
+
+  //creo un elemento html div
+  let cardTxt=document.createElement('div');
+  //aggiungo al div la classe card-text
+  cardTxt.classList.add("card-text");
+
+  //creo 2 elementi html, un h3 e un p
+  let htmlName=document.createElement("h3");
+  let htmlRole=document.createElement('p');
+    //riempio i due elementi con i parametri della funzione
+    htmlName.innerHTML=`${name}`;
+    htmlRole.innerHTML=`${role}`;
+  
+  //inserisco nel div cardImg l'elemento img
+  cardImg.append(htmlImg);
+  //inserisco nel div cardTxt i 2 elementi h3 e p
+  cardTxt.append(htmlName, htmlRole);
+  
+  //inserisco in teamCard i 2 div generati sopra
+  teamCard.append(cardImg, cardTxt);
+
+  return teamCard
+}
+
 const team = [
     {
       name: 'Wayne Barnett',
@@ -30,6 +74,8 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+  const container=document.querySelector('.team-container');
   
   //ciclo l'array team di oggetti
   for(let i=0; i<team.length; i++){
@@ -43,5 +89,5 @@ const team = [
 
     //stampo con un console.log per verificare
     console.log(nameMember, roleMember, imgMember)
-
+    container.append(newCard(nameMember, roleMember, imgMember));
   }
